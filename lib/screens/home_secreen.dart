82664d0097662d9d736e244c78756c70ff2bfcad
komol112 <nabiyevkomol2@gmail.com>
2025-06-30 17:07:01 +0500

    model , provider, servicelar tayyor

--- lib/screens/home_secreen.dart
+++ lib/screens/home_secreen.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:kino_top/view_model/movie_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeSecreen extends StatefulWidget {
   const HomeSecreen({super.key});
@@ -9,7 +13,28 @@ class HomeSecreen extends StatefulWidget {
 
 class _HomeSecreenState extends State<HomeSecreen> {
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    context.read<MovieProvider>().getMovie();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("homeScreen")));
+    return Consumer<MovieProvider>(
+      builder: (context, movieprovider, child) {
+        log(
+          movieprovider.movieModel?.results?[0].title ??
+              "malumot olishda xatolik",
+        );
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              movieprovider.movieModel?.results?[1].id.toString() ??
+                  "malumot yoq",
+            ),
+          ),
+        );
+      },
+    );
   }
 }
